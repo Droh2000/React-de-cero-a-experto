019896894a1fb1b52d38807a867503c21a78dd4c
diego ortega <diego-chido2011@hotmail.com>
2024-12-06 16:37:13 -0600

    useSelector y useDispatch

--- 09-toolkit-redux/src/App.jsx
+++ 09-toolkit-redux/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from './store/slices/counter/counterSlice';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Requerimos leer algo del Store para eso esta el UseSelector, dentro de este tenemos una funcion
+  // un Callback comun y corriente que tiene como primer argumento el state y luego de ahi regresamos 
+  // lo que nos interesa (Aqui le estamos diciendo que del State tome todo el objeto del "counter")
+  // de ahi desestructuramos el Counter (Que es el que definimos dentro del "initialState" en el archivo "counterSlice")
+  const { counter } = useSelector( state => state.counter );
+
+  // Hagamos el dispatch de una nueva accion que es la de incrementar que exportamos en el archivo "counterSlice"
+  // ahora ya no tenemos que pensar en el Tipo de la accion o el Payload sino que simplemente tomamos la accion
+  // asi que tenemos que ejecutar un Dispatch (Este "dispach" ya sabe a que reducer va a llegar, ya sabe lo que nesecita)
+  // debido a que la funcion se creo desde el Slide
+  const dispach = useDispatch();
 
   return (
     <>
@@ -18,8 +29,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        {/*
+            Cuando se hace click en el boton mandamos a llamar el Dispatch y la accion a ejecutar es la de increment
+            como una funcion para que se ejecute (Con esto ya va a funcionar el contador con Redux)
+         */}
+        <button onClick={() => dispach( increment() )}>
+          count is {counter}
         </button>
       </div>
     </>
