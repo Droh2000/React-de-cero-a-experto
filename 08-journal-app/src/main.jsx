a0549385a5c07939b9aa250c2ed8efaba6d60bf5
diego ortega <diego-chido2011@hotmail.com>
2024-12-09 18:22:20 -0600

    Configuracion de Redux

--- 08-journal-app/src/main.jsx
+++ 08-journal-app/src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { JournalApp } from './JournalApp'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store } from './store/store'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* Para configurar las rutas */}
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    {/* Implementacion del store y el patron Redux */}
+    <Provider store={ store }>
+      {/* Para configurar las rutas */}
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
