6107dd7331af375202bd0f57da67dd752f9c037b
diego ortega <diego-chido2011@hotmail.com>
2024-12-18 11:40:36 -0600

    Poner nota en estado Activo

--- 08-journal-app/src/journal/components/SideBarItem.jsx
+++ 08-journal-app/src/journal/components/SideBarItem.jsx
@@ -1,8 +1,21 @@
 import { TurnedInNot } from '@mui/icons-material';
 import { ListItem, ListItemButton, ListItemIcon, Grid2, ListItemText } from '@mui/material';
 import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { setActiveNote } from '../../store/journal/journalSlice';
 
-export const SideBarItem = ({ title, body, id }) => {
+export const SideBarItem = ({ title = '', body, id, date, imageUrls = [] }) => {
+
+    // Para hacer que al dar click en una nota se ponga en estado Activo esa nota
+    // esto es sincrono por tanto no usamos nigun thonk, ya tenemos la nota por tanto solo
+    // la mandamos a activar
+    const dispatch = useDispatch();
+
+    const onClickNote = () => {
+        // El setActiveNote esta esperando un payload que es la nota que debe de tener todo lo que estamos mandando 
+        // como argumento desde el SideBar.jsx
+        dispatch( setActiveNote({ title, body, id, date, imageUrls }) );
+    }
 
     // Para el caso que el titulo sea muy largo lo ponemos asi para que no pase de una linea
     const newTitle = useMemo(() => {
@@ -14,7 +27,7 @@ export const SideBarItem = ({ title, body, id }) => {
 
     return (
         <ListItem disablePadding>
-            <ListItemButton>
+            <ListItemButton onClick={onClickNote}>
                 <ListItemIcon>
                     <TurnedInNot />
                 </ListItemIcon>
