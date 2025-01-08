0136131b1966770923cd3a8d9e23a1cd59493c74
diego ortega <diego-chido2011@hotmail.com>
2025-01-08 16:55:18 -0600

    resolver lo de non-serializable value was detected

--- 10-calendar/src/store/store.js
+++ 10-calendar/src/store/store.js
@@ -10,5 +10,15 @@ export const store = configureStore({
         // Despues de implementado el reducer se lo mandamos
         ui: uiSlice.reducer,
         calendar: calendarSlice.reducer
-    }
+    },
+    // Para resolver lo de non-serializable value was detected
+    // Esto paso porque intenta serializar la fecha de tipo String de JS
+    // y eso no es recomendable pero el tipo de dato Date si es valido en JS
+    // lo que pasa es que no deberiamos de mandar la fecha directamente si no generarla como
+    // un numero y trabajarla asi, en este caso vamos a hacer la configuracion en 
+    // el Store de Middlewares en Redux 
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        // Asi evitamos que las fechas las revise si las puede serializar
+        serializableCheck: false, 
+    }),
 });
