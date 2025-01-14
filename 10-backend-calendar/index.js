e086ef0bc9492862601af7c38de683e00b769a8d
diego ortega <diego-chido2011@hotmail.com>
2025-01-14 12:11:58 -0600

    Vaariable de entorno y carpeta Publica

--- 10-backend-calendar/index.js
+++ 10-backend-calendar/index.js
@@ -4,25 +4,38 @@
 // Asi funciona en Node las importaciones
 const express = require('express');
 
+// Para que use las variables de Entorno
+require('dotenv').config();
+
+// Ver todos los procesos que estan corriendo en Node
+// console.log( process.env ); -> Aqui veremos las variables de entorno que definamos
+
 // Crear el servidor de Express
 const app = express();
 
 // Configuracion de Rutas
 // Le ponemos el tipo de peticion que queremos
 // La funcion se dispara con los argumentos de Request y Response
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     // Cuando se solicite esta ruta esto es lo que le queremos responder
-    console.log('Se requere /'); // Esto lo veremos al acceder a la ruta "loccalhost:4000" y en la consola veremos el mensaje
+    //console.log('Se requere /'); // Esto lo veremos al acceder a la ruta "loccalhost:4000" y en la consola veremos el mensaje
 
     // Mostramos una respuesta
     res.json({
         ok: true
     });
-});
+
+});*/
+
+// Directorio Publico
+// El "use()" en express es conocido como Middleware (Funcion que se ejecuta cuando se hace una peticion al servidor)
+// En este caso establecemos el directorio publico que es lo que se mostrara
+app.use( express.static('public') );
+
 
 // Escuchar peticiones
 // El primer argumento es el puerto en donde correra y el segundo argumento es la funcion que se ejecuta
-// cuando el servidor este arrancando
-app.listen( 4000, () => {
-    console.log(`Servidro corriendo en puerto ${4000}`);
+// cuando el servidor este arrancando (Aqui usamos la variable de entorno y para ver los cambios tenemos que detener y volver a ejecutar la APP)
+app.listen( process.env.PORT, () => {
+    console.log(`Servidro corriendo en puerto ${process.env.PORT}`);
 });
